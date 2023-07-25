87d09799160520833a3211d12a8048e17464ecd9
CarvalhovictorH <hugo.vco.dev@gmail.com>
2023-07-25 19:22:13 -0300

    implementando o método POST

--- src/controllers/listarController.js
+++ src/controllers/listarController.js
@@ -1,9 +1,47 @@
-const livros = require("../bancodedados");
+const { livros } = require("../bancodedados");
+let { identificadorLivros } = require("../bancodedados");
 
 const listarLivros = (req, res) => {
   return res.status(200).json(livros);
 };
 
+const listarPorId = (req, res) => {
+  const { id } = req.params;
+  const livro = livros.find((livro) => {
+    return livro.id === Number(id);
+  });
+
+  if (!livro) {
+    return res.status(404).json({ mensagem: "Livro não encontrado." });
+  }
+
+  res.status(201).json(livro);
+};
+
+const cadastrarLivros = (req, res) => {
+  const { nome, autor } = req.body;
+
+  if (!nome) {
+    res.status(400).json({ mensagem: "O nome não foi enviado." });
+  }
+
+  if (!autor) {
+    res.status(400).json({ mensagem: "O autor não foi enviado." });
+  }
+
+  const livro = {
+    id: identificadorLivros++,
+    nome,
+    autor,
+    estoque: true,
+  };
+
+  livros.push(livro);
+  return res.status(201).json(livro);
+};
+
 module.exports = {
   listarLivros,
+  listarPorId,
+  cadastrarLivros,
 };
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const rotas = express();
-const { listarLivros } = require("../controllers/listarController");
+
+const {
+  listarLivros,
+  listarPorId,
+  cadastrarLivros,
+} = require("../controllers/listarController");
 
 rotas.get("/livros", listarLivros);
+rotas.get("/livros/:id", listarPorId);
+rotas.post("/livros", cadastrarLivros);
 
 module.exports = rotas;
